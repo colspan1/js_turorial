474e34ec2f2c323501b75e4160ffdb448d3283c7
colspan1 <negip5489@gmail.com>
2024-10-12 07:30:05 +0000

    start container config

---
+++ 01_Basic/test.js
@@ -0,0 +1,9 @@
+// we use .js in javascript file rather than .txt because when we inject these file in a software(compailer, interpriter) for output the software's check the source code(code) and file extension(.js) on those basics it give us result //
+
+console.log("hello world");  //print hello world
+
+// in early  days of javascript we use to add javascript in html (<script>) to get result in browser because the software(compiler, interpriter) for exicution was not easily accessable 
+//but in recent days stand alon software(compiler, interpriter ) for exicution was introduced
+//runtime enviroment for js is node js and deno js. the main lead developed and founder is ryal dhal
+
+// in this tutorail i am using githun(codespace) as a runtime enviroment for js
