b4ef6ed8e9ca262d2ce6e7c75584a44118600cff
Priyanshu Negi <negip5489@gmail.com>
2024-10-19 06:27:49 +0000

    random

--- 01_Basic/08_nums_and_maths.js
+++ 01_Basic/08_nums_and_maths.js
@@ -38,3 +38,6 @@ console.log(Math.random()) //random number is between 0 and 1
 console.log((Math.random()*10)+1) //some time value is 0 so we add +1
 console.log(Math.floor(Math.random()*10)+1)
 
+let min = 10;
+let max = 20;
+console.log(Math.floor(Math.random()*(max - min +1))+ min)
